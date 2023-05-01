2ccb61994a6623413ceb4d6a8f00026c2e01af19
aalok0003 <81555853+aalok0003@users.noreply.github.com>
2023-05-01 12:46:33 +0530

    Update moviemovie.py
    optimization

--- Movie recommender/moviemovie.py
+++ Movie recommender/moviemovie.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 22 11:14:43 2023
-
-@author: alok
-"""
+# import some necessary libraries for performing some NLP and machine learning tasks
 import streamlit as st
 import pickle
 import difflib 
@@ -12,91 +7,56 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import pandas as pd
 
-
+# This code reads a CSV file containing the movie data and stores it in a pandas DataFrame called movies_data.
 movies_data=pd.read_csv("movies (1).csv")
 
-selected_features=['genres','keywords','tagline','cast','director'] # imdb, adult rating,etc
-
+# This code fills any missing values in the selected features (genres, keywords, tagline, cast, and director) with an empty string.
+selected_features=['genres','keywords','tagline','cast','director']
 for feature in selected_features:
     movies_data[feature]= movies_data[feature].fillna('')
 
+# This code concatenates the values of the selected features into a single column called combined_features.
 combined_features= movies_data['genres']+''+movies_data['keywords']+''+movies_data['tagline']+''+movies_data['cast']+''+movies_data['director']
 
+# This code uses the TfidfVectorizer function from scikit-learn to convert the textual data in combined_features to numerical data, and then computes the cosine similarity between all pairs of movies based on their feature vectors.
 vectorizer = TfidfVectorizer()
 feature_vector = vectorizer.fit_transform(combined_features)
-
 similarity= cosine_similarity(feature_vector)
 
+# This code creates a list of all movie titles.
 lsit_of_all_titles= movies_data['title'].tolist()
 
-
-
-
-
-
-
-
-
-
-
-
+# This code defines a function called recommend that takes a movie name as input and returns a list of 30 recommended movies based on their similarity score to the input movie.
 def recommend(movie_name):
-    #print('Other Recommendations: \n')
-
-    
     i=1
     recommendations=[]
     for movie in sorted_similar_movie_list:
         index=movie[0]
         title_from_index = movies_data[movies_data.index==index]['title'].values[0]
-
-    ## suggest 30  movies
         if (i<30):
             recommendations.append(title_from_index)
             i+=1
     return recommendations 
 
+# This code loads three pickle files containing precomputed data required for the recommendation system: a list of all movie titles (movielist.pkl), a sorted list of similar movies for each movie in the dataset (sorted_similar_movie.pkl), and the original movie data (movies_data.pkl).
 movies_list= pickle.load(open("movielist.pkl",'rb'))
 sorted_similar_movie_list= pickle.load(open("sorted_similar_movie.pkl",'rb'))
-title_from_index= pickle.load(open("title_from_index.pkl",'rb'))
-similarity_score= pickle.load(open("similarity_score.pkl",'rb'))
-similarity= pickle.load(open("similarity.pkl",'rb'))
 movies_data= pickle.load(open("movies_data.pkl",'rb'))
 
-input_movie_index= pickle.load(open("input_movie_index.pkl",'rb'))
-close_match_list= pickle.load(open("close_match_list.pkl",'rb'))
-closest_match= pickle.load(open("closest_match.pkl",'rb'))
-combined_features= pickle.load(open("combined_features.pkl",'rb'))
-vectorizer= pickle.load(open("vectorizer.pkl",'rb'))
-combined_features= pickle.load(open("combined_features.pkl",'rb'))
-feature_vector= pickle.load(open("feature_vector.pkl",'rb'))
-
+# This code sets up the Streamlit app and allows the user to select a movie from a dropdown list. It then finds the closest match to the selected movie in the dataset and retrieves the similarity scores between the selected movie and all other movies.
 st.title("Movie Recommender System")
 
 selected_movie = st.selectbox("Chose a movie here", movies_list)
 
 close_match_list = difflib.get_close_matches(selected_movie,lsit_of_all_titles)
-
 closest_match = close_match_list[0]
-
 input_movie_index= movies_data[movies_data.title == closest_match]['index'].values[0]
 
 similarity_score= list(enumerate(similarity[input_movie_index ]))
-
-## sorting the movies based on similarity score
 sorted_similar_movie_list=sorted(similarity_score, key= lambda x:x[1], reverse=True)
 
-## x[0]==index, x[1]= similarity score
-
-
-
-
-
-
-
-
+# This code displays a "Recommend" button in the Streamlit app
 if st.button('Recommend'):
-    
     recommended_movies= recommend(selected_movie)
     for j in recommended_movies:
         st.write(j)
